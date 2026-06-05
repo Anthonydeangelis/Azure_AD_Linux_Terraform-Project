9558f58b257ef27c01033f07229775ef974bf68a
antgotgithub <117580657+antgotgithub@users.noreply.github.com>
2026-06-04 22:28:50 -0400

    Yaml edit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "lab_rg" {
     location = var.location  # Azure region (e.g., eastus, westus2)
 
     tags = {
-        Environment = "Pipineline-TestNum31"
+        Environment = "PipinelineYamlUpdate"
         Project     = "AD_Linux_Projects"
     }
 }
